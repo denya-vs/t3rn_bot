0a75fbda7667844454edef686933855e149a8753
Denys <v.denya04@gmail.com>
2024-09-06 20:41:05 -0400

    randomise every trx

--- src/core/transaction.ts
+++ src/core/transaction.ts
@@ -1,18 +1,16 @@
-import {Wallet, parseUnits} from 'ethers';
-import {CONFIG} from '../config/config';
-import {NetworkOption, NetworkManager} from './network';
-import {getRandomValue, getRandomDelay} from '../utils/delay';
-import {ApiService} from '../services/ApiService';
-import {Logger} from '../utils/logger';
+import { Wallet, parseUnits } from 'ethers';
+import { CONFIG } from '../config/config';
+import { NetworkOption, NetworkManager } from './network';
+import { getRandomValue, getRandomDelay } from '../utils/delay';
+import { ApiService } from '../services/ApiService';
+import { Logger } from '../utils/logger';
 
 export class TransactionManager {
     private wallet: Wallet;
-    private networkOption: NetworkOption;
     private apiService: ApiService;
 
     constructor(wallet: Wallet) {
         this.wallet = wallet;
-        this.networkOption = NetworkManager.getRandomNetwork();
         this.apiService = new ApiService();
     }
 
@@ -24,7 +22,10 @@ export class TransactionManager {
 
         while (remainingTx > 0) {
             try {
-                const amount = await this.getAmount();
+                // Выбор случайной сети перед каждой транзакцией
+                const networkOption = NetworkManager.getRandomNetwork();
+
+                const amount = await this.getAmount(networkOption);
                 if (!amount) {
                     Logger.error(`[ ${new Date().toLocaleTimeString()} ] Failed to get the amount. Skipping transaction...`);
                     continue;
@@ -34,7 +35,7 @@ export class TransactionManager {
                 const randomDelay = getRandomDelay(CONFIG.MIN_DELAY_MS, CONFIG.MAX_DELAY_MS);
                 const gasPrice = parseUnits('0.1', 'gwei');
                 const transaction = {
-                    data: this.transactionData(this.wallet.address, amount, this.networkOption),
+                    data: this.transactionData(this.wallet.address, amount, networkOption),
                     to: CONFIG.CONTRACT_ADDRESS,
                     gasLimit: CONFIG.GAS_LIMIT,
                     gasPrice,
@@ -42,24 +43,23 @@ export class TransactionManager {
                     value: parseUnits('0.0001', 'ether'),
                 };
 
-                Logger.info(`[ ${new Date().toLocaleTimeString()} ] Sending transaction from ${this.wallet.address}...`);
+                Logger.info(`[ ${new Date().toLocaleTimeString()} ] Sending transaction from ${this.wallet.address} to ${NetworkManager.getNetworkName(networkOption)}...`);
 
                 const txStartTime = new Date();
                 const result = await this.wallet.sendTransaction(transaction);
 
                 Logger.info(`[ ${new Date().toLocaleTimeString()} ] Waiting for transaction to be mined...`);
 
-                // Проверка на null перед использованием
                 const receipt = await this.wallet.provider?.waitForTransaction(result.hash);
                 if (receipt && receipt.status === 1) {
                     const txEndTime = new Date();
-                    Logger.success(`[ ${txEndTime.toLocaleTimeString()} ] Transaction confirmed successfully from Arbitrum Sepolia to ${NetworkManager.getNetworkName(this.networkOption)} Sepolia!`);
+                    Logger.success(`[ ${txEndTime.toLocaleTimeString()} ] Transaction confirmed successfully from Arbitrum Sepolia to ${NetworkManager.getNetworkName(networkOption)} Sepolia!`);
                     Logger.info(`[ ${txEndTime.toLocaleTimeString()} ] Transaction hash: https://sepolia-explorer.arbitrum.io/tx/${result.hash}`);
                     Logger.info(`[ ${txEndTime.toLocaleTimeString()} ] Transaction took ${txEndTime.getTime() - txStartTime.getTime()} ms`);
 
                     successfulTx++;
                 } else {
-                    Logger.error(`[ ${new Date().toLocaleTimeString()} ] Transaction failed from Arbitrum Sepolia to ${NetworkManager.getNetworkName(this.networkOption)} Sepolia!: https://sepolia-explorer.arbitrum.io/tx/${result.hash}`);
+                    Logger.error(`[ ${new Date().toLocaleTimeString()} ] Transaction failed from Arbitrum Sepolia to ${NetworkManager.getNetworkName(networkOption)} Sepolia!: https://sepolia-explorer.arbitrum.io/tx/${result.hash}`);
                 }
                 console.log('');
 
@@ -83,15 +83,15 @@ export class TransactionManager {
         return successfulTx;
     }
 
-    private async getAmount(): Promise<string | null> {
-        return this.apiService.getAmount(this.networkOption);
+    private async getAmount(networkOption: NetworkOption): Promise<string | null> {
+        return this.apiService.getAmount(networkOption);
     }
 
     private transactionData(address: string, amount: string, network: NetworkOption): string {
         const chainPrefix = network === NetworkOption.Base ? '0x56591d5962737370' :
             network === NetworkOption.Blast ? '0x56591d59626c7373' :
                 network === NetworkOption.Optimism ? '0x56591d596f707370' :
-                    '0x0000000000000000'; // Значение по умолчанию, если сеть не определена
+                    '0x0000000000000000';
 
         return `${chainPrefix}000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000${address.slice(2)}0000000000000000000000000000000000000000000000000000${amount.slice(2)}0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000005af3107a4000`;
     }
